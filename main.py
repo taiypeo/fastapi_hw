fb3e576b30ed2ac59246d49d48a1f8de533dc4cd
taiypeo <Qwertygid@gmail.com>
2023-10-25 01:27:25 +0300

    Add solution

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from enum import Enum
-from fastapi import FastAPI
+
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -23,25 +25,62 @@ class Timestamp(BaseModel):
 
 
 dogs_db = {
-    0: Dog(name='Bob', pk=0, kind='terrier'),
-    1: Dog(name='Marli', pk=1, kind="bulldog"),
-    2: Dog(name='Snoopy', pk=2, kind='dalmatian'),
-    3: Dog(name='Rex', pk=3, kind='dalmatian'),
-    4: Dog(name='Pongo', pk=4, kind='dalmatian'),
-    5: Dog(name='Tillman', pk=5, kind='bulldog'),
-    6: Dog(name='Uga', pk=6, kind='bulldog')
+    0: Dog(name="Bob", pk=0, kind="terrier"),
+    1: Dog(name="Marli", pk=1, kind="bulldog"),
+    2: Dog(name="Snoopy", pk=2, kind="dalmatian"),
+    3: Dog(name="Rex", pk=3, kind="dalmatian"),
+    4: Dog(name="Pongo", pk=4, kind="dalmatian"),
+    5: Dog(name="Tillman", pk=5, kind="bulldog"),
+    6: Dog(name="Uga", pk=6, kind="bulldog"),
 }
 
-post_db = [
-    Timestamp(id=0, timestamp=12),
-    Timestamp(id=1, timestamp=10)
-]
+post_db = [Timestamp(id=0, timestamp=12), Timestamp(id=1, timestamp=10)]
 
 
-@app.get('/')
+@app.get("/")
 def root():
     # ваш код здесь
-    ...
+    return Response()
+
 
 # ваш код здесь
-...
+@app.post("/post")
+def post():
+    last_post = post_db[-1] if len(post_db) > 0 else Timestamp(id=-1)
+    next_post = Timestamp(id=last_post.id + 1, timestamp=int(time.time()))
+    post_db.append(next_post)
+    return next_post
+
+
+@app.get("/dog")
+def get_dogs(kind: DogType):
+    return [dog for dog in dogs_db.values() if dog.kind == kind]
+
+
+@app.post("/dog")
+def create_dog(dog: Dog):
+    if dog.pk in dogs_db:
+        # As per the group chat
+        raise HTTPException(status_code=409, detail="The specified PK already exists.")
+
+    dogs_db[dog.pk] = dog
+    return dog
+
+
+@app.get("/dog/{pk}")
+def get_dog_by_pk(pk: int):
+    if pk not in dogs_db:
+        raise HTTPException(
+            status_code=404,
+            detail={
+                "msg": f"No PK {pk} found.",
+            },
+        )
+
+    return dogs_db[pk]
+
+
+@app.patch("/dog/{pk}")
+def update_dog_by_pk(pk: int, dog: Dog):
+    dogs_db[pk] = dog
+    return dog
